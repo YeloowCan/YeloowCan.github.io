b560750204daa8c9bc679df7eeb106d15629b98b
YeloowCan <942789838@qq.com>
2023-05-07 17:17:19 +0800

    Site updated: 2023-05-07 17:17:18

--- js/codeBLang.js
+++ js/codeBLang.js
@@ -1,20 +1,11 @@
-// 代码块语言识别
+function load_language() {
+    var blocks = $('code.hljs');
+    for (var i = 0; i < blocks.length; i++) {
+        var span = document.createElement('span');
+        span.classList.add('codeblock-language');
+        span.innerHTML = blocks[i].classList[1];
+        blocks[i].insertBefore(span, blocks[i].children[0]);
+    }
+}
 
-$(function () {
-  var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
-
-  $('pre').after($highlight_lang);
-  $('pre').each(function () {
-    var code_language = $(this).attr('class');
-
-if (!code_language) {
-  return true;
-};
-var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim();
-
-// 首字母大写
-lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
-$(this).siblings(".code_lang").text(lang_name);
-
-  });
-});
+$(document).ready(load_language);
